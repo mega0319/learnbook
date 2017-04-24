10ae55baa0044ba88429d232e1b5974ccfae6738
Naz Khan <nazmuskhan@yahoo.com>
2017-04-23 21:49:23 -0400

    increased post photo size

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :dislikes
   has_many :comments
 
-  has_attached_file :image, styles: { large: "600x600", medium: "300x300", thumbnail: "150x150"}, default_url: "images/no_user_logo.jpeg"
+  has_attached_file :image, styles: { large: "600x600", medium: "450x450", thumbnail: "150x150" }, default_url: "images/no_user_logo.jpeg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
